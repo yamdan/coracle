6343295221585d958ac49a596f2acce8fa6ad2f5
Dan Yamamoto <dan@iij.ad.jp>
2024-07-24 23:36:53 +0000

    feat: Attach Verifiable Presentation to NoteContentKind1 component (TBD)

---
+++ src/app/shared/VerifiablePresentation.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Anchor from "src/partials/Anchor.svelte"
+  import {router} from "src/app/util/router"
+
+  export let url
+  export let pubkey
+</script>
+
+<Anchor 
+  modal
+  stopPropagation
+  href={router.at("media").of(url).toString()}>
+  <span>🖊️</span>
+</Anchor>
